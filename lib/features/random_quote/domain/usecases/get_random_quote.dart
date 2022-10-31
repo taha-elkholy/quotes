d946923789c0872caccca70e4a54be6253df4e98
taha-elkholy <taha-elkholy>
2022-10-31 18:08:51 +0200

    complete domain layer

---
+++ lib/features/random_quote/domain/usecases/get_random_quote.dart
@@ -0,0 +1,15 @@
+import 'package:quotes/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:quotes/core/usecase/usecase.dart';
+import 'package:quotes/features/random_quote/domain/entities/quote.dart';
+import 'package:quotes/features/random_quote/domain/repositories/quote_repository.dart';
+
+class GetRandomQuote extends UseCase<Quote, NoParam> {
+  final QuoteRepository _quoteRepository;
+
+  GetRandomQuote(this._quoteRepository);
+
+  @override
+  Future<Either<Failure, Quote>> call(NoParam param) =>
+      _quoteRepository.getRandomQuote();
+}
